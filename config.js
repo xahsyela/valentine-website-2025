9ae409cbda44e8134b7bdfa57458e7f31e7afe3a
xahsyela <asraan91@gmail.com>
2026-02-05 16:08:09 +0800

    Update page title for Valentine's theme

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Hi baby, Will You Be My Valentine? 💖",
+    pageTitle: "Hi baby! I made something for you 🩷",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
